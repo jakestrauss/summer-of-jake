dbdaa1c251e59e8694bf9a77e116618a05ecfec8
jakestrauss <jake.strauss217@gmail.com>
2021-10-26 18:33:00 -0700

    Initial map setup with sample strava kmls

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { GoogleMap, useLoadScript, KmlLayer } from '@react-google-maps/api';
+import mapStyles from "./mapStyles";
+
+const mapContainerStyle = {
+  width: '100vw',
+  height: '100vh'
+};
+const center = {
+  lat: 40.644366,
+  lng: -39.872822
+};
+const options = {
+  styles: mapStyles,
+  disableDefaultUI: true,
+  zoomControl: true
+}
 
 function App() {
+  const {isLoaded, loadError} = useLoadScript({
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY || ""
+  })
+
+  if (loadError) return (<>Error loading maps</>);
+  if (!isLoaded) return (<>Loading maps</>);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Summer of Jake Adventure Map</h1>
+      <GoogleMap 
+        mapContainerStyle={mapContainerStyle} 
+        zoom={3}
+        center={center}
+        options={options}
+      >
+
+        <KmlLayer url="https://storage.googleapis.com/strava-kmls/mergedKML_1635298225660.kml" />
+      </GoogleMap>
     </div>
   );
 }
