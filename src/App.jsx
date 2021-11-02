224e26bb54ba389244f818fd9184e9ea38811c15
jakestrauss <jake.strauss217@gmail.com>
2021-11-02 12:10:23 -0700

    Converted back to javascript project from Typescript

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,10 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { useJsApiLoader } from '@react-google-maps/api';
 import Map from "./components/Map";
-
 function App() {
-  const {isLoaded, loadError} = useJsApiLoader({
+    const { isLoaded, loadError } = useJsApiLoader({
         googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY || ""
     });
-  return (
-    <Map
-      isLoaded={isLoaded}
-      loadError={loadError}
-      />
-  );
+    return (<Map isLoaded={isLoaded} loadError={loadError}/>);
 }
-
 export default App;
